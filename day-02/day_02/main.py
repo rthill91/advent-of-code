789cd78b165d1f760d8e20acdfb09416dbe7ea33
Ryan Hill <rthill91@gmail.com>
2019-12-02 09:55:51 -0600

    day2.1 done

--- day-02/day_02/main.py
+++ day-02/day_02/main.py
@@ -1,6 +1,39 @@
 def part1():
-    raise NotImplementedError
+    opcodes = {
+        1: _add,
+        2: _multiply,
+    }
+
+    with open('input') as fh:
+        instructions = [int(inst) for inst in fh.readline().split(',')]
+
+    instructions[1] = 12
+    instructions[2] = 2
+    index = 0
+    while index < len(instructions):
+        inst = instructions[index]
+        if inst == 99:
+            break
+        op = opcodes.get(inst, None)
+        op(index, instructions)
+        index += 4
+    print(instructions)
+
 
 
 def part2():
     raise NotImplementedError
+
+
+def _add(index, instructions):
+    oper1 = instructions[index+1]
+    oper2 = instructions[index+2]
+    dest = instructions[index+3]
+    instructions[dest] = instructions[oper1] + instructions[oper2]
+
+
+def _multiply(index, instructions):
+    oper1 = instructions[index+1]
+    oper2 = instructions[index+2]
+    dest = instructions[index+3]
+    instructions[dest] = instructions[oper1] * instructions[oper2]
